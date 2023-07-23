cfd5b2c3f47819360b7fc4255f3a16c85e5d4d51
yznahmad jar <jaradatgamer@gmail.com>
2023-07-23 09:42:37 +0000

    #

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,19 @@
+<script>
+
+
+
+
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+
+
+	$: {console.log('count is ... '+ count)}
+
+
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
